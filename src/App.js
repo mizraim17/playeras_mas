37da34d5b74b477d660c2599ff8e5335d9ac43b5
kaneki <cecilia.arrgo@gmail.com>
2021-08-12 17:20:53 -0500

    add new products

--- src/App.js
+++ src/App.js
@@ -1,68 +1,96 @@
-import React, { useState } from "react";
+import React, { useDebugValue, useState } from "react";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Row, Container,Form,FormControl, Button, Navbar, Nav, Brand, Link } from "react-bootstrap";
+import {
+	Row,
+	Container,
+	Form,
+	FormControl,
+	Button,
+	Navbar,
+	Nav,
+} from "react-bootstrap";
 
 import Article from "./Component/Articles/Article";
 import bdArticulos from "./articulos.json";
 
 function App() {
+	let eliminarDiacriticosEs = (texto) => {
+		let text = texto
+			.normalize("NFD")
+			.replace(
+				/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+				"$1"
+			)
+			.normalize();
+
+		return text;
+	};
 
 	const search = (e) => {
-			console.log("=>", e.target.value)
-		setword(e.target.value);
-		};
-		
-		const [word, setword] = useState('');
-	
-	return (
+		setword(eliminarDiacriticosEs(e.target.value));
+	};
 
-		< > 
-			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+	const [word, setword] = useState("");
+
+	return (
+		<>
+			<Navbar
+				collapseOnSelect
+				expand="lg"
+				bg="dark"
+				variant="dark"
+				sticky="top"
+			>
 				<Navbar.Brand href="#home">Playeras & Más</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  			<Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
-				<Nav className="mr-auto">
-					<Nav.Link href="#home">Inventario</Nav.Link>
-					<Nav.Link href="#features">Tienda</Nav.Link>
-					{/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-				</Nav>
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Inventario</Nav.Link>
+						<Nav.Link href="https://www.mecanicojeans.mx/">Tienda</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
 
-				<Form inline >
-					<FormControl type="text" placeholder="Búsqueda" className="mr-sm-2"
+				<Form inline>
+					<FormControl
+						type="text"
+						placeholder="Búsqueda"
+						className="mr-sm-2"
 						onChange={search}
 					/>
 					<Button variant="outline-info">Buscar</Button>
 				</Form>
-			</Navbar>				 	
- 
-		<Container>
-			
-			<Row>
- 
-				{	word!=''?
-				
-				bdArticulos
-					.filter(el => el.Nombre.toLowerCase().includes(word.toLowerCase()))
-					.map((el, i) => {
-					return <Article key={i} item={el} />;
-					})
-				:
-					bdArticulos
-						.sort(function (a, b) {
-							if ( a.Nombre < b.Nombre )
-							return -1;
-							if ( a.Nombre > b.Nombre )
-								return 1;
-							return 0;
-							})
-						.map((el, i) => {
-					return <Article key={i} item={el} />;
-				})}
-			</Row>
-		</Container>
-			
+			</Navbar>
+
+			<Container>
+				<Row>
+					{word != ""
+						? bdArticulos
+								.filter((el) =>
+									el.Nombre.normalize("NFD")
+										.replace(
+											/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+											"$1"
+										)
+										.normalize()
+										.toLowerCase()
+										.includes(word.toLowerCase())
+								)
+								.map((el, i) => {
+									return <Article key={i} item={el} />;
+								})
+						: bdArticulos
+								.sort(function (a, b) {
+									if (a.Nombre < b.Nombre) return -1;
+									if (a.Nombre > b.Nombre) return 1;
+									return 0;
+								})
+								.map((el, i) => {
+									return <Article key={i} item={el} />;
+								})}
+				</Row>
+			</Container>
 		</>
 	);
 }
